0171080a9a5063f06d9e062791d5583384fe4a7a
DuongThan <ngoxuanduong230896@gmail.com>
2017-02-21 22:52:34 +0700

    Finish custom service

---
+++ AngularJS/AngularJS/Execute/Custom service/factory.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="D:\Learn Web\AngularJS\AngularJS\AngularJS\Script/angular.min.js" />
+
+/// <reference path="script.js" />
+myApp.factory('stringService', function () {
+    return {
+        processString: function (input) {
+            if (!input) {
+                return;
+            }
+            var outPut = "";
+            for (var i = 0; i < input.length; i++) {
+                if (input[i] == input[i].toUpperCase())
+                    outPut += " ";
+                outPut += input[i];
+            }
+            return outPut;
+        }
+    };
+});
--- AngularJS/AngularJS/Execute/Custom service/script.js
+++ AngularJS/AngularJS/Execute/Custom service/script.js
@@ -1,7 +1,9 @@
 ﻿/// <reference path="D:\Learn Web\AngularIS\AngularJS\AngularJS\Script/angular.min.js" />
 
 var myApp = angular.module("main", [])
-                    .controller("controller", function ($scope) {
-
-
+                    .controller("controller", function ($scope,stringService) {
+                        $scope.transformstring = function(input)
+                        {
+                            $scope.output = stringService.processString(input);
+                        }
                     });
